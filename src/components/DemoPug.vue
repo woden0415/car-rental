e76bd9a908479aa8517c730671c786b6ede94cf9
王冬 <wangdong7871@100.me>
2023-05-24 16:13:22 +0800

    feat: [项目初始化]

---
+++ src/components/DemoPug.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+const addCount = () => {
+  count.value += 1
+}
+const reduceCount = () => {
+  count.value -= 1
+}
+</script>
+
+<template lang="pug">
+div.red-color() {{ count  }}
+button(@click="addCount") addCount
+button(@click="reduceCount") reduceCount
+</template>
+
+<style scoped>
+</style>
